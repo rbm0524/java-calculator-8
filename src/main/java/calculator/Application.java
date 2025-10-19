e9d06bdb193f735175bd11758b55d268cccfc1e4
rbm0524 <rbm0524@naver.com>
2025-10-19 20:27:58 +0900

    feat: 입력, 메인 로직, 출력 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,25 @@
 package calculator;
 
+import calculator.domain.Calculator;
+import calculator.domain.Regex;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args) throws IllegalArgumentException {
+        // 입력받기
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        int result = 0;
+
+        // 메인 로직
+        Regex regex = new Regex();
+        Calculator calculator = new Calculator();
+        ArrayList<Integer> numbers = regex.analyze_string(input);
+        result = calculator.calc(numbers);
+
+        // 출력하기
+        System.out.println("결과 : " + result);
     }
 }
