9f840dd81d7196dce4f5bcde055b8b76755a430d
rbm0524 <rbm0524@naver.com>
2025-10-20 10:42:50 +0900

    test(ApplicationTest) : 정상 테스트와 예외 테스트 케이스 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,13 +1,53 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
+    @Test
+    void 빈_문자열() {
+        assertSimpleTest(() -> {
+            run("\n"); // 사용자가 엔터키만 눌렀을 때 아무 문자열이 없게 된다.
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 숫자_문자열() {
+        assertSimpleTest(() -> {
+            run("3434");
+            assertThat(output()).contains("결과 : 3434");
+        });
+    }
+
+    @Test
+    void 콜론_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1:2:3:4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 쉼표_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2,3,4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 콜론_쉼표_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:3,4:5");
+            assertThat(output()).contains("결과 : 15");
+        });
+    }
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -17,12 +57,96 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 커스텀_구분자_여러개_사용() {
+        assertSimpleTest(() -> {
+            run("//;[\\n1;2[3;4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_여러개_사용2() {
+        assertSimpleTest(() -> {
+            run("//;[)\\n1;2[3;4)5");
+            assertThat(output()).contains("결과 : 15");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자가_빈_문자열() {
+        assertSimpleTest(() -> {
+            run("//\\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자가_빈_문자열2() {
+        assertSimpleTest(() -> {
+            run("//\\n123");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+
+    @Test
+    void 음수_예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    void 덧셈_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,a,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 정의되지_않은_구분자_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1.2.3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    // 커스텀 구분자를 정의했다면, 해당 구분자를 사용하도록 합니다.
+    @Test
+    void 커스텀_구분자_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//&\\n1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    // 커스텀 구분자를 정의했다면, 해당 구분자를 사용하도록 합니다.
+    @Test
+    void 커스텀_구분자_예외_테스트2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//&\\n1,2&3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 구분자로_끝나는_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2,3,"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 구분자로_끝나는_예외_테스트2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//&\\n1&2&3&"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
 
     @Override
     public void runMain() {
